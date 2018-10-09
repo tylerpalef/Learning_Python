ff1acb005b90575fce9d2d63921a0a5805163c7e
“Tyler <tylerpalef@gmail.com>
2018-10-09 14:50:24 -0400

    completed second lesson regarding variables and data types

---
+++ 02_variables.py
@@ -0,0 +1,30 @@
+# Defining an integer
+
+myint = 7
+print(myint)
+
+# Float
+
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+# String
+
+mystring = 'hello'
+print(mystring)
+mystring = "hello" # Better if you plan to use apostrophes
+print(mystring)
+
+# Simple operators
+
+one = 1
+two = 2
+three = one + two
+print(three)
+
+hello = "hello"
+world = "world"
+helloworld = hello + " " + world
+print(helloworld)
