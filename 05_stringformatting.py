04c66d6ef11917efc9f6667f70906453a43f7f10
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:46:33 -0400

    added an example of multiple interpolations in one line

--- 05_stringformatting.py
+++ 05_stringformatting.py
@@ -1,3 +1,8 @@
 # This prints out "Hello, John!"
 name = "John"
 print("Hello, %s!" % name)
+
+# This prints out "John is 23 years old."
+name = "John"
+age = 23
+print("%s is %d years old." % (name, age)) #s for name, #d for number of age
