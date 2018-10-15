b5a8811c3eb7abd697d7dc152c57f12b8cc1a3f2
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:11:35 -0400

    completed the list tutorial on the append method as well as using for loops in Python

--- 03_lists.py
+++ 03_lists.py
@@ -4,7 +4,7 @@
 # mylist.append(3)
 # print(mylist[0]) # prints 1
 #
-# # prints out the full array in a for loop
+# # prints out the full array in a for loop on each own line
 # for x in mylist:
 #     print(x)
 
@@ -12,15 +12,23 @@
 # In this exercise, you will need to add numbers and strings to the correct lists using the "append" list method. You must add the numbers 1,2, and 3 to the "numbers" list, and the words 'hello' and 'world' to the strings variable.
 #
 # You will also have to fill in the variable second_name with the second name in the names list, using the brackets operator []. Note that the index is zero-based, so if you want to access the second item in the list, its index will be 1.
-# numbers = []
-# strings = []
-# names = ["John", "Eric", "Jessica"]
+numbers = []
+numbers.append(1)
+numbers.append(2)
+numbers.append(3)
+strings = []
+strings.append('hello')
+strings.append('world')
+names = ["John", "Eric", "Jessica"]
 
 # write your code here
-second_name = None
+second_name = names[1]
 
 
 # this code should write out the filled arrays and the second name in the names list (Eric).
-print(numbers)
-print(strings)
-print("The second name on the names list is %s" % second_name)
+for x in numbers:
+    print(x)
+
+print(strings) # All list items on one line
+
+print("The second name on the names list is %s" % second_name) # String interpolation represented by %s
