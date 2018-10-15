72451231cc4c5d68877b9d5d030ef5ce8c1ee761
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:47:01 -0400

    example of using a list in a string

--- 05_stringformatting.py
+++ 05_stringformatting.py
@@ -6,3 +6,7 @@ print("Hello, %s!" % name)
 name = "John"
 age = 23
 print("%s is %d years old." % (name, age)) #s for name, #d for number of age
+
+# This prints out: A list: [1, 2, 3]
+mylist = [1,2,3]
+print("A list: %s" % mylist)
