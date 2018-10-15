2f8d3bae3940411be0b804e4d43d1e4ead53b6bc
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:48:01 -0400

    added a list of basic argument specifiers

--- 05_stringformatting.py
+++ 05_stringformatting.py
@@ -1,12 +1,19 @@
-# This prints out "Hello, John!"
-name = "John"
-print("Hello, %s!" % name)
+# # This prints out "Hello, John!"
+# name = "John"
+# print("Hello, %s!" % name)
+#
+# # This prints out "John is 23 years old."
+# name = "John"
+# age = 23
+# print("%s is %d years old." % (name, age)) #s for name, #d for number of age
+#
+# # This prints out: A list: [1, 2, 3]
+# mylist = [1,2,3]
+# print("A list: %s" % mylist)
 
-# This prints out "John is 23 years old."
-name = "John"
-age = 23
-print("%s is %d years old." % (name, age)) #s for name, #d for number of age
-
-# This prints out: A list: [1, 2, 3]
-mylist = [1,2,3]
-print("A list: %s" % mylist)
+# Here are some basic argument specifiers you should know:
+    # %s - String (or any object with a string representation, like numbers)
+    # %d - Integers
+    # %f - Floating point numbers
+    # %.<number of digits>f - Floating point numbers with a fixed amount of digits to the right of the dot.
+    # %x/%X - Integers in hex representation (lowercase/uppercase)
