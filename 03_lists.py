ca5563624edf84fb785b98f96588fd098e645ff0
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:05:39 -0400

    commented regarding the for loop

--- 03_lists.py
+++ 03_lists.py
@@ -3,9 +3,7 @@ mylist.append(1)
 mylist.append(2)
 mylist.append(3)
 print(mylist[0]) # prints 1
-print(mylist[1]) # prints 2
-print(mylist[2]) # prints 3
 
-# prints out 1,2,3
+# prints out the full array in a for loop
 for x in mylist:
     print(x)
