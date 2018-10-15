1c4b8bdae3612af1389204b8a325e1a87f8dc72f
“Tyler <tylerpalef@gmail.com>
2018-10-15 14:06:15 -0400

    commented out the initial exercise

--- 03_lists.py
+++ 03_lists.py
@@ -1,9 +1,9 @@
-mylist = []
-mylist.append(1)
-mylist.append(2)
-mylist.append(3)
-print(mylist[0]) # prints 1
-
-# prints out the full array in a for loop
-for x in mylist:
-    print(x)
+# mylist = []
+# mylist.append(1)
+# mylist.append(2)
+# mylist.append(3)
+# print(mylist[0]) # prints 1
+#
+# # prints out the full array in a for loop
+# for x in mylist:
+#     print(x)
