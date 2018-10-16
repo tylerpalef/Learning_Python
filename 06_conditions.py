1ccdc4bb0ccf016e3ae95538e206d72754c57053
“Tyler <tylerpalef@gmail.com>
2018-10-16 10:39:17 -0400

    added an example of and as well as or boolean operators

--- 06_conditions.py
+++ 06_conditions.py
@@ -1,4 +1,14 @@
-x = 2
-print(x == 2) # prints out True
-print(x == 3) # prints out False
-print(x < 3) # prints out True
+# True and false boolean
+# x = 2
+# print(x == 2) # prints out True
+# print(x == 3) # prints out False
+# print(x < 3) # prints out True
+
+# Boolean Operators
+name = "John"
+age = 23
+if name == "John" and age == 23:
+    print("Your name is John, and you are also 23 years old.")
+
+if name == "John" or name == "Rick":
+    print("Your name is either John or Rick.")
