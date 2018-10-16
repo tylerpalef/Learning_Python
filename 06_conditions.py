b8a257f86cafafbff81a1bc765cefaec02798065
“Tyler <tylerpalef@gmail.com>
2018-10-16 10:41:14 -0400

    added an example of proper spacing for if statement code blocks

--- 06_conditions.py
+++ 06_conditions.py
@@ -4,11 +4,24 @@
 # print(x == 3) # prints out False
 # print(x < 3) # prints out True
 
-# Boolean Operators
-name = "John"
-age = 23
-if name == "John" and age == 23:
-    print("Your name is John, and you are also 23 years old.")
+# Boolean Operators: and | or
+# name = "John"
+# age = 23
+# if name == "John" and age == 23:
+#     print("Your name is John, and you are also 23 years old.")
+#
+# if name == "John" or name == "Rick":
+#     print("Your name is either John or Rick.")
 
-if name == "John" or name == "Rick":
-    print("Your name is either John or Rick.")
+# # Boolean Operator: n
+# name = "John"
+# if name in ["John", "Rick"]:
+#     print("Your name is either John or Rick.")
+#
+
+# # Code Blocks
+# x = 2
+# if x == 2:
+#     print("x equals two!")
+# else:
+#     print("x does not equal to two.")
