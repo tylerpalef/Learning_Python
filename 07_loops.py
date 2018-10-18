9d01337bef4d7d8e5a6fa1f83c8b64ec98a44592
“Tyler <tylerpalef@gmail.com>
2018-10-18 15:07:38 -0400

    added examples of using range

--- 07_loops.py
+++ 07_loops.py
@@ -1,3 +1,15 @@
-primes = [2, 3, 5, 7]
-for prime in primes:
-    print(prime)
+# primes = [2, 3, 5, 7]
+# for prime in primes:
+#     print(prime)
+
+# Prints out the numbers 0,1,2,3,4
+for x in range(5): # starts at 0, counts up until 5
+    print(x)
+
+# Prints out 3,4,5
+for x in range(3, 6):
+    print(x)
+
+# Prints out 3,5,7
+for x in range(3, 8, 2):
+    print(x)
