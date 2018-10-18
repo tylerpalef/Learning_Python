25967872c2eadcba4d1b2d79e22e47cb702bd236
“Tyler <tylerpalef@gmail.com>
2018-10-18 15:14:56 -0400

    an example of an else statement in a python loop

--- 07_loops.py
+++ 07_loops.py
@@ -17,15 +17,32 @@
 #     print(count) # Prints out 0,1,2,3,4
 #     count += 1  # This is the same as count = count + 1
 
-count = 0
-while True:
-    print(count) # Prints out 0,1,2,3,4
-    count += 1
-    if count >= 5:
-        break
+# count = 0
+# while True:
+#     print(count) # Prints out 0,1,2,3,4
+#     count += 1
+#     if count >= 5:
+#         break
+#
+# for x in range(10):
+#     # Check if x is even
+#     if x % 2 == 0: # Prints out only odd numbers - 1,3,5,7,9
+#         continue
+#     print(x)
 
-for x in range(10):
-    # Check if x is even
-    if x % 2 == 0: # Prints out only odd numbers - 1,3,5,7,9
-        continue
-    print(x)
+
+# Prints out 0,1,2,3,4 and then it prints "count value reached 5"
+
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print("count value reached %d" %(count))
+
+for i in range(1, 10):
+    if(i%5==0):
+        break
+    print(i) # Prints out 1,2,3,4
+else:
+    print("this is not printed because for loop is terminated because of break but not due to fail in condition")
