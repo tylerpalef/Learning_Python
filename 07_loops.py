85d4c6a6ce11a59986ebd04f2f733757e069f868
“Tyler <tylerpalef@gmail.com>
2018-10-18 15:10:10 -0400

    cleaned up the files to show what the print result would be in-line with the print code line

--- 07_loops.py
+++ 07_loops.py
@@ -2,21 +2,17 @@
 # for prime in primes:
 #     print(prime)
 
-# # Prints out the numbers 0,1,2,3,4
 # for x in range(5): # starts at 0, counts up until 5
-#     print(x)
-#
-# # Prints out 3,4,5
-# for x in range(3, 6):
-#     print(x)
-#
-# # Prints out 3,5,7
-# for x in range(3, 8, 2):
-#     print(x)
+#     print(x) # Prints out the numbers 0,1,2,3,4
 
-# Prints out 0,1,2,3,4
+# for x in range(3, 6):
+#     print(x) # # Prints out 3,4,5
+
+# for x in range(3, 8, 2):
+#    print(x) # Prints out 3,5,7
+
 
 count = 0
 while count < 5:
-    print(count)
+    print(count) # Prints out 0,1,2,3,4
     count += 1  # This is the same as count = count + 1
